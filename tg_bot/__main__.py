8998e341ba40d16d1986ea78e789f59127cd04f5
chintu7577 <70334906+chintu7577@users.noreply.github.com>
2020-10-08 17:53:40 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,11 +19,39 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 
-Hello {}, Iam a bot and my name is {}. Im a group manager bot.
-No use in messaging me here am available on @Telugumovie_rocks
+Hello {}, Iam a bot and my name is {}😎 Im a group manager bot maintained by  [RoHiTH reDDY](tg://user?id={}).
+
+No use in messaging me here am available on @BTECHTHOPULU
+
 **U r not able to do any thing in this so be silent.**
 
 """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
